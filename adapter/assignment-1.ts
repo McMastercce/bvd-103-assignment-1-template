68de4ff6169abcef48d4702c235d43d4931defbc
Per Pettersson <Per.Pettersson@gmail.com>
2025-12-24 11:49:05 -0500

    Updated packages and Router for listing books

--- adapter/assignment-1.ts
+++ adapter/assignment-1.ts
@@ -10,17 +10,20 @@ export interface Book {
 
 // If you have multiple filters, a book matching any of them is a match.
 async function listBooks(filters?: Array<{from?: number, to?: number}>) : Promise<Book[]>{
-    // if (!filters || filters.length === 0) {
-    //     return books; // No filters, return all books
-    // }
-    // console.log("running listBooks")
-    // return books.filter(book =>
-    //     filters.some(filter =>
-    //         (filter.from === undefined || book.price >= filter.from) &&
-    //         (filter.to === undefined || book.price <= filter.to)
-    //     )
-    // );
-    throw new Error("Todo")
+  //     const query = filters?.map(({ from, to }, index) => {
+  //     let result = ''
+  //     if (typeof from === 'number') {
+  //       result += `&filters[${index}][from]=${from}`
+  //     }
+  //     if (typeof to === 'number') {
+  //       result += `&filters[${index}][to]=${to}`
+  //     }
+  //     return result
+  //   }).join('&') ?? ''
+   
+  // Use the browser fetch API to make a request to the /books endpoint with the constructed query string.
+  // fetch(`http://localhost:3000/books?${query}`);
+  throw new Error("Todo")
 }
 
 const assignment = "assignment-1";
